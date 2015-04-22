08ce7b40c8ffbf189af06f36e8ab4f520a8e0c9e
ordepmalo <bastospeedro@gmail.com>
2015-04-22 18:18:55 -0300

    modifying FSM in interface.vhd

--- rtl/vhdl/interface/interface.vhd
+++ rtl/vhdl/interface/interface.vhd
@@ -6,7 +6,7 @@
 -- Author        : Pedro Messias Jose da Cunha Bastos
 -- Company       : 
 -- Created       : 2015-04-17
--- Last update   : 2015-04-17
+-- Last update   : 2015-04-22
 -- Target Device : 
 -- Standard      : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -28,16 +28,15 @@ entity interface is
   generic (
     MAX_VALUE      : natural := 32;
     MAX_VALUE_BITS : natural := 5
-
     );
 
   port (
-    sysclk        : in  std_logic;
-    reset_n       : in  std_logic;
-    data_i        : in  std_logic_vector(31 downto 0);
-    data_serial_o : out std_logic;
-    stb_o         : out std_logic;
-    clk           : out std_logic
+    sysclk  : in  std_logic;
+    reset_n : in  std_logic;
+    en_i    : in  std_logic;
+    ctrl_o  : in  std_logic;
+    stb_o   : out std_logic;
+    clk     : out std_logic
     );
 
 end interface;
@@ -46,12 +45,10 @@ architecture interface_rtl of interface is
 
   type INTERFACE_ST_TYPE is (ST_INIT, ST_INCR, ST_VERIFY);
 
-  signal state_reg    : INTERFACE_ST_TYPE;
-  signal state_next   : INTERFACE_ST_TYPE;
-  signal counter_reg  : unsigned(MAX_VALUE_BITS - 1 downto 0);
-  signal counter_next : unsigned(MAX_VALUE_BITS - 1 downto 0);
-  signal clk_divider  : std_logic;
-  signal prescaler    : unsigned (23 downto 0);
+  signal state_reg  : INTERFACE_ST_TYPE;
+  signal state_next : INTERFACE_ST_TYPE;
+  signal count_reg  : unsigned(MAX_VALUE_BITS - 1 downto 0);
+  signal count_next : unsigned(MAX_VALUE_BITS - 1 downto 0);
 
 begin
 
@@ -59,53 +56,46 @@ begin
 
   begin
 
-    state_reg   <= state_next;
-    counter_reg <= counter_next;
-
     if reset_n = '0' then
-      state_reg   <= ST_INIT;
-      counter_reg <= (others => '0');
-      clk_divider <= '0';
-      prescaler   <= (others => '0');
+      state_reg <= ST_INIT;
+      count_reg <= (others => '0');
     elsif rising_edge(sysclk) then  -- sysclk = 50.000.000 (frequency of FPGA on DE2 module)
-      if prescaler = 23 then
-        prescaler   <= (others => '0');
-        clk_divider <= not clk_divider;
-      else
-        prescaler <= prescaler + 1;
-      end if;
-
-      state_reg   <= state_next;
-      counter_reg <= counter_next;
-
+    else
     end if;
 
-  end process;
+    state_reg <= state_next;
+    count_reg <= count_next;
 
-  process(state_reg, counter_reg)
+  end if;
 
-  begin
+end process;
 
-    state_next   <= state_reg;
-    counter_next <= counter_reg;
+process(state_reg, count_reg)
 
-    case state_reg is
+begin
 
-      when ST_INIT =>
-        state_next <= ST_INCR;
+  state_next <= state_reg;
+  count_next <= count_reg;
 
-      when ST_INCR =>
-        counter_next <= counter_reg + 1;
-        state_next   <= ST_VERIFY;
+  case state_reg is
 
-      when ST_VERIFY =>
-        if counter_reg >= MAX_VALUE then
-          state_next <= ST_INIT;
-        else state_next <= ST_INCR;
-        end if;
+    when ST_INIT =>
+      state_next <= ST_INIT;
 
-    end case;
+    when ST_INCR =>
+      if en_i = '1' then
+        count_next <= count_reg + 1;
+      end if;
+      state_next <= ST_VERIFY;
 
-  end process;
+    when ST_VERIFY =>
+      if count_reg >= MAX_VALUE then
+        state_next <= ST_INIT;
+      else state_next <= ST_INCR;
+      end if;
+
+  end case;
+
+end process;
 
 end interface_rtl;
